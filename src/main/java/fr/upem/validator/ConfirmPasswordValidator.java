e6cb0110283c8ab11e40f989ac55ad24988939c8
Denis <Denis@PC-Denis>
2015-02-27 14:22:01 +0100

    validator

---
+++ src/main/java/fr/upem/validator/ConfirmPasswordValidator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.upem.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author Denis
+ */
+@FacesValidator("confirmPasswordValidator")
+public class ConfirmPasswordValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String password = (String) value;
+        String confirm = (String) component.getAttributes().get("confirm");
+
+        if (password == null || confirm == null) {
+            return; // Just ignore and let required="true" do its job.
+        }
+
+        if (!password.equals(confirm)) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Passwords are not equal.", null));
+        }
+
+    }  
+}
