1ea37b5e3fc6ed816c8aaea940dc328f74382f57
Miguel <Miguel@Miguel-Bureau>
2015-03-05 12:52:25 +0100

    Ajout de la vérification javascript pour l'inscription Ajout des pages backoffice : - connexion : index.html - affichage des utilisateurs : utilisateur-liste.html - recherche des utilisateurs : utilisateur-recherche.html

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,23 +1,4 @@
 $(document).ready(function(){
-    /*$('.image-container').empty().justifiedImages({
-        images :    [{"url_s": "file:///C:/Users/Miguel/Desktop/Upemgur/images/sample/1.jpg",
-                  "width_s": "550" ,
-                  "height_s": "412"}],
-        rowHeight: 200,
-        maxRowHeight: 400,
-        thumbnailPath: function(photo, width, height){
-            var purl = photo.url_s;
-            if( photo.url_n && (width > photo.width_s * 1.2 || height > photo.height_s * 1.2) ) purl = photo.url_n;
-            if( photo.url_m && (width > photo.width_n * 1.2 || height > photo.height_n * 1.2) ) purl = photo.url_m;
-            if( photo.url_z && (width > photo.width_m * 1.2 || height > photo.height_m * 1.2) ) purl = photo.url_z;
-            if( photo.url_l && (width > photo.width_z * 1.2 || height > photo.height_z * 1.2) ) purl = photo.url_l;
-            return purl;
-        },
-        getSize: function(photo){
-            return {width: photo.width_s, height: photo.height_s};
-        },
-        margin: 1
-    });*/
     $(".image-container").justifiedGallery({
         rowHeight : 200,
         margins : 3,
@@ -26,4 +7,56 @@ $(document).ready(function(){
                             visibleOpacity : 0.7,
                             nonVisibleOpacity : 0.0}
     });
+    
+    $("#subscriptionForm\\:inputEmail").keyup(checkEmail);
+    $("#subscriptionForm\\:inputLogin").keyup(checkLogin);
+    //$("#inputPassword").blur(checkPassword());
+    $("#subscriptionForm\\:inputPassword").keyup(checkPassword);
+    $("#subscriptionForm\\:confirmPassword").keyup(checkPassword);
 });
+
+function checkEmail() {
+    if(validEmail($("#subscriptionForm\\:inputEmail").val())) {
+        $("#subscriptionForm\\:inputEmail").parent().removeClass("has-error");
+        //$("#badEmail").addClass("sr-only");
+    }
+    else {
+        $("#subscriptionForm\\:inputEmail").parent().addClass("has-error");
+        //$("#badEmail").removeClass("sr-only");
+    }
+}
+
+function checkLogin() {
+    var re= /^[^\W\d_]\w*$/;
+    if(($("#subscriptionForm\\:inputLogin").val().length >= 3)
+            && (re.test($("#subscriptionForm\\:inputLogin").val()) )) {
+        $("#subscriptionForm\\:inputLogin").parent().removeClass("has-error");
+        //$("#badLogin").addClass("sr-only");
+    }
+    else {
+        $("#subscriptionForm\\:inputLogin").parent().addClass("has-error");
+        //$("#badLogin").removeClass("sr-only");
+    }
+}
+
+function checkPassword() {
+    if(($("#subscriptionForm\\:inputPassword").val() == $("#subscriptionForm\\:confirmPassword").val())
+            &&($("#subscriptionForm\\:inputPassword").val().length >= 4)) {
+        $("#subscriptionForm\\:inputPassword").parent().removeClass("has-error");
+        $("#subscriptionForm\\:confirmPassword").parent().removeClass("has-error");
+    }
+    else {
+        $("#subscriptionForm\\:inputPassword").parent().addClass("has-error");
+        $("#subscriptionForm\\:confirmPassword").parent().addClass("has-error");
+    }
+}
+
+function checkInscription() {
+    var validForm = false;
+
+}
+
+function validEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
